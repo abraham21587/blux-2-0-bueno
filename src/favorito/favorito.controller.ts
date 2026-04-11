48523445bb463a089e09ab6dd498510308e2f992
ManuelRiascos <manuel.riascos2008@gmail.com>
2026-04-11 18:42:23 -0500

    favoritos controller modificado

--- src/favorito/favorito.controller.ts
+++ src/favorito/favorito.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Query, Body } from '@nestjs/common';
+import { FavoritoService } from './favorito.service';
 
-@Controller('favorito')
-export class FavoritoController {}
+@Controller('v1/favoritos')
+export class FavoritoController {
+  constructor(private readonly favoritoService: FavoritoService) {}
+
+  @Get('mis-favoritos')
+  getFavoritos(@Query('correo') correo: string) {
+    return this.favoritoService.getFavoritos(correo);
+  }
+
+  @Post('agregar')
+  agregarFavorito(@Body() body: { correo: string; contenidoId: string }) {
+    return this.favoritoService.agregarFavorito(body.correo, body.contenidoId);
+  }
+
+  @Delete('eliminar')
+  eliminarFavorito(@Body() body: { correo: string; contenidoId: string }) {
+    return this.favoritoService.eliminarFavorito(body.correo, body.contenidoId);
+  }
+}
