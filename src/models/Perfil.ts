c4a19ab3e9480a368d30adc20ea2cd5248c1cdd4
abraham21587 <abrahamwaggon821@gmail.com>
2026-04-11 16:45:32 -0500

    correcion de lineas

--- src/models/Perfil.ts
+++ src/models/Perfil.ts
@@ -1,19 +1,22 @@
 import { Schema, model, Document } from 'mongoose';
 
 export interface IPerfil extends Document {
-    nombre: string;
-    avatarUrl: string;
-    usuarioCorreo: string;
-    esMenor: boolean;
-    pin?: string;
+  nombre: string;
+  avatarUrl: string;
+  usuarioCorreo: string;
+  esMenor: boolean;
+  pin?: string;
 }
 
-const PerfilSchema = new Schema<IPerfil>({
-    nombre:        { type: String, required: true, trim: true },
-    avatarUrl:     { type: String, default: 'https://i.pravatar.cc/150' },
+const PerfilSchema = new Schema<IPerfil>(
+  {
+    nombre: { type: String, required: true, trim: true },
+    avatarUrl: { type: String, default: 'https://i.pravatar.cc/150' },
     usuarioCorreo: { type: String, required: true, lowercase: true },
-    esMenor:       { type: Boolean, default: false },
-    pin:           { type: String, default: '' }
-}, { timestamps: true });
+    esMenor: { type: Boolean, default: false },
+    pin: { type: String, default: '' },
+  },
+  { timestamps: true },
+);
 
 export default model<IPerfil>('Perfil', PerfilSchema);
