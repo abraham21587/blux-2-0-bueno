26f4b39205d2f4055064221a9e93b2d3a2e822b4
juanmig7890 <juanmig2708m@gmail.com>
2026-04-11 18:51:28 -0500

    admin

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AdminService } from './admin.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+import { UsuarioSchema } from '../models/Usuario';
+import { ContenidoSchema } from '../models/Contenido';
+import { FavoritoSchema } from '../models/Favorito';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Usuario', schema: UsuarioSchema },
+      { name: 'Contenido', schema: ContenidoSchema },
+      { name: 'Favorito', schema: FavoritoSchema }
+    ])
+  ],
+  controllers: [AdminController],
   providers: [AdminService],
-  controllers: [AdminController]
 })
 export class AdminModule {}
